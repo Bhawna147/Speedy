295ab86e898ed931458a2e25bb5ccbf991353269
aparna-bhatt <aparnabhatt2001@gmail.comk>
2021-11-26 20:20:26 +0530

    signin2

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Signin = () => {
     return (
         <div>
-            <p> this is signin </p>
+            <p> this is signin fjhkdsfhkhfjk</p>
             <button>signup</button>
         </div>
     )
