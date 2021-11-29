adc4daf59440368380a40dab2f610d45acf0ba6d
Bhawna147 <Khatribhawna77@gmail.com>
2021-11-29 14:15:42 +0530

    individual-typing-test

---
+++ frontend/src/components/text/Preview.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./text.css";
+
+
+const Preview = () => {
+
+  
+    return (
+        <div>
+
+
+               <div className="text-display" id="textDisplay">
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus cupiditate quo laboriosam, odio tempora vitae quibusdam placeat necessitatibus voluptates? Natus tempora optio repellat quam saepe nulla ratione eos ducimus officiis.
+                 </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Preview
